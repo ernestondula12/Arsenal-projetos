dc2e7348d5e9d9c513533ccf24ca3f27dc2db36e
Ernesto Ndula <meteusrosa704@gmail.com>
2025-01-23 00:48:04 +0100

    Fiz uma alteração no arquivo data.js para poder colocar as tecnologias usada em cada um dos projetos listados nesse arsenal de projetos

--- src/data.js
+++ src/data.js
@@ -3,28 +3,28 @@ const projetos = [
       id: 1,
       title: 'Sistema de gestão de membros',
       img: './images/gestaoMembros.png',
-      description: "Um sistema que visa gerenciar todos os membros da igreja",
+      description: "Um sistema que visa gerenciar todos os membros da igreja, neste projeto trabalhei com Bootstrap, Nodejs e MongoDB",
       site: "https://github.com/ernestondula12/Sistema-de-gest-o-de-membros.git"
     },
     {
       id: 2,
       title: 'Site de pedidos de comida',
       img: './images/DelciasdaBanda.png',
-      description: 'Um site especifico para gerenciamento de uma pizzaria ou restaurente',
+      description: 'Um site especifico para gerenciamento de uma pizzaria ou restaurente, usei tecnologias como React, SASS E PostgreSQL',
       site: ""
     },
   {
     id: 3,
     title: 'BarberShop',
     img: './images/BarberShop.png',
-    description: 'Uma Landing Page para barbearias que querem destacar o seu negocio na internet',
+    description: 'Uma Landing Page para barbearias que querem destacar o seu negocio na internet criei este site usando SASS e uma biblioteca animação para ScroolReveal',
     site: "https://projeto-barbearia-lovat-beta.vercel.app/"
   },
   {
     id: 4,
     title: "MindAdvice",
     img: './images/mindadvice.png',
-    description: 'Consumindo API que gera conselhos aleatorios após o clique no botão',
+    description: 'Consumindo API que gera conselhos aleatorios após o clique no botão, criei esta pequeno site para testar as minhas habilidades com React e Bootstrap',
     site: "https://mindadvice.vercel.app/"
   },
   {
@@ -38,7 +38,7 @@ const projetos = [
     id: 6,
     title: "Amazon Best Sellers",
     img: './images/AbestSeller.png',
-    description: 'Uma pequena aplicação de clonagem da Amazon para mostrar os livros mais vendidos',
+    description: 'Uma pequena aplicação de clonagem da Amazon para mostrar os livros mais vendidos,fiz esta aplicação para testar as minhas habilidades com React,na estruturação de pastas e componentes',
     site: "https://best-sellers-lyart.vercel.app/"
   },
   ]
